101c2038889e23bc5e9e9e7b474231ff42c14f3c
Alexander Poole <alexanderp@opera.com>
2024-03-07 06:58:39 +0100

    Properly load dynamic env variables

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -1,10 +1,9 @@
 import { createDirectus, rest, type FetchInterface, staticToken } from '@directus/sdk';
-import { SECRET_CMS_URL, SECRET_CMS_API_KEY } from '$env/static/private';
+import { env } from '$env/dynamic/private';
 import type { BlogSchema } from './types';
 
 export function directus(fetch: FetchInterface) {
-  console.log(SECRET_CMS_URL);
-  return createDirectus<BlogSchema>(SECRET_CMS_URL, { globals: { fetch } })
-    .with(staticToken(SECRET_CMS_API_KEY))
+  return createDirectus<BlogSchema>(env.SECRET_CMS_URL, { globals: { fetch } })
+    .with(staticToken(env.SECRET_CMS_API_KEY))
     .with(rest());
 }

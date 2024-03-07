c78a8824764d6e6f971ddc49a79c7e75f7707462
Alexander Poole <alexanderp@opera.com>
2024-03-07 06:56:25 +0100

    Log cms url to see that env is correctly setup

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -3,6 +3,7 @@ import { SECRET_CMS_URL, SECRET_CMS_API_KEY } from '$env/static/private';
 import type { BlogSchema } from './types';
 
 export function directus(fetch: FetchInterface) {
+  console.log(SECRET_CMS_URL);
   return createDirectus<BlogSchema>(SECRET_CMS_URL, { globals: { fetch } })
     .with(staticToken(SECRET_CMS_API_KEY))
     .with(rest());
